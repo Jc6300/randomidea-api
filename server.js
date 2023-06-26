1ffabdc16396ede3e9a6ead1509071c76004ed8b
Jc6300 <joshcotton93@gmail.com>
2023-06-26 17:36:19 -0500

    setup mongobd and mongoose

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const express = require("express");
-const port = 5000;
+require("dotenv").config();
+const port = process.env.port || 5000;
+const connectDB = require("./config/db");
 const app = express();
+
+connectDB();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
